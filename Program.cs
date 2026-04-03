0af701efee2fd1c50ff13981227394c65f3dd426
ukishere <61353722+ukishere@users.noreply.github.com>
2026-04-03 12:32:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.IO.Pipelines;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string check;
+        int first_number;
+        int second_number;
+        string logic_operator;
+
+        while(true)
+        {
+            Console.WriteLine("Enter first number:");
+
+            if (!Int32.TryParse(Console.ReadLine(), out first_number))
+            {
+                Console.WriteLine("Not a number");
+                continue;
+            }
+
+            Console.WriteLine("Enter second number:");
+
+            if (!Int32.TryParse(Console.ReadLine(), out second_number))
+            {
+                Console.WriteLine("Not a number");
+                continue;
+            }
+
+            Console.WriteLine("Enter operator:");
+
+            logic_operator = Console.ReadLine();
+
+            switch(logic_operator[0])
+            {
+                case '+':
+                    Console.WriteLine("Result\n{0} + {1} = {2}", first_number, second_number, first_number+second_number );
+                    break;
+
+                case '-':
+                    Console.WriteLine("Result\n{0} - {1} = {2}", first_number, second_number, first_number-second_number );
+                    break;
+
+                case '*':
+                    Console.WriteLine("Result\n{0} * {1} = {2}", first_number, second_number, first_number*second_number );
+                    break;
+
+                case '/':
+                    Console.WriteLine("Result\n{0} / {1} = {2}", first_number, second_number, first_number/second_number );
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong operator");
+                    break;
+            }
+
+        Console.WriteLine("Type yes to continue");
+        check = Console.ReadLine();
+
+        if(check != "yes") return;
+        }
+    }
+}
