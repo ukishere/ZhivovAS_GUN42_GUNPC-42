a670ff0cc246b913b99e2ed5e0ad0ae88ba1270a
ukishere <61353722+ukishere@users.noreply.github.com>
2026-04-07 13:15:26 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,99 +1,106 @@
-﻿using System.IO.Pipelines;
+﻿using System.Formats.Asn1;
+using System.IO.Pipelines;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string check;
-        int first_number;
-        int second_number;
-        int result;
-        string logic_operator;
+        //Task A1
+        Console.WriteLine("Task A1");
 
-        while(true)
+        int[] a1 = new int[8]; 
+        a1[0] = 0;
+        a1[1] = 1;
+
+        Console.Write("{0} {1} ", a1.GetValue(0), a1.GetValue(1));
+
+        for (int i = 2; i <= 7; i++)
         {
-            Console.WriteLine("Enter first number:");
+            a1[i] = a1[i-1] + a1[i-2];
+            Console.Write("{0} ", a1.GetValue(i));
+        }
 
-            if (!Int32.TryParse(Console.ReadLine(), out first_number))
+        // Task A2
+        Console.WriteLine("\n\nTask A2");
+
+        string[] a2 = new string[11] {
+                                        "January", 
+                                        "February", 
+                                        "March", 
+                                        "April", 
+                                        "May", 
+                                        "June", 
+                                        "July", 
+                                        "August", 
+                                        "September",
+                                        "November",
+                                        "December"
+                                        };
+     
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.Write("{0} ", a2.GetValue(i));
+        } 
+
+        //Task A3
+        Console.WriteLine("\n\nTask A3");
+
+        int[,] a3 = new int[3,3];
+        a3[0,0] = 2;
+        a3[0,1] = 3;
+        a3[0,2] = 4;
+
+        Console.WriteLine("{0} {1} {2}", a3[0,0], a3[0,1], a3[0,2]);
+
+        for (int i = 1; i<= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
             {
-                Console.WriteLine("Not a number");
-                continue;
+                a3[i,j] = a3[i-1,j] * a3[0,j];
+                Console.Write("{0} ", a3[i,j]);  
             }
+            Console.WriteLine();           
+        }
 
-            Console.WriteLine("Enter second number:");
+        //Task A4
+        Console.WriteLine("\nTask A4");
 
-            if (!Int32.TryParse(Console.ReadLine(), out second_number))
+        double[] a41 = new double[] {1,2,3,4,5};
+        double[] a42 = new double[] {Math.E, Math.PI};
+        double[] a43 = new double[] {Math.Log10(1), Math.Log10(10), Math.Log10(100), Math.Log10(1000)};
+
+        double[][] a4 = new double[3][] {a41, a42, a43};
+
+        for (int i = 0; i<= 2; i++)
+        {
+            for (int j = 0; j <= a4[i].Length-1; j++)
             {
-                Console.WriteLine("Not a number");
-                continue;
+                Console.Write("{0} ", a4[i][j]);
             }
+            Console.WriteLine();           
+        } 
 
-            Console.WriteLine("Enter operator:");
+        //Task B1
+        Console.WriteLine("\nTask B1");
 
-            logic_operator = Console.ReadLine();
+        int[] array = { 1, 2, 3, 4, 5 }; 
+        int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
 
-            switch(logic_operator[0])
-            {
-                case '&':
-                    result = first_number & second_number;
+        Array.Copy(array, array2, 3); 
 
-                    Console.WriteLine("(Decimal) {0} & {1} = {2}", first_number, second_number, result);
+        for (int i=0; i <= array2.Length-1; i++)
+        {
+            Console.Write("{0} ", array2[i]);
+        }
 
-                    Console.WriteLine("(Binary) {0} & {1} = {2}", 
-                    Convert.ToString(first_number, 2), 
-                    Convert.ToString(second_number, 2), 
-                    Convert.ToString(result, 2)); 
+        //Task B2
+        Console.WriteLine("\n\nTask B2");
 
-                    Console.WriteLine("(Hexadecimal) {0} & {1} = {2}", 
-                    Convert.ToString(first_number, 16), 
-                    Convert.ToString(second_number, 16), 
-                    Convert.ToString(result, 16));
+        Array.Resize(ref array, array.Length*2);
 
-                    break;
-
-                case '|':
-                    result = first_number | second_number;
-                    
-                    Console.WriteLine("(Decimal) {0} | {1} = {2}", first_number, second_number, result);
-
-                    Console.WriteLine("(Binary) {0} | {1} = {2}", 
-                    Convert.ToString(first_number, 2), 
-                    Convert.ToString(second_number, 2), 
-                    Convert.ToString(result, 2)); 
-
-                    Console.WriteLine("(Hexadecimal) {0} | {1} = {2}", 
-                    Convert.ToString(first_number, 16), 
-                    Convert.ToString(second_number, 16), 
-                    Convert.ToString(result, 16));
-
-                    break;
-
-                case '^':
-                    result = first_number ^ second_number;
-                    
-                    Console.WriteLine("(Decimal) {0} ^ {1} = {2}", first_number, second_number, result);
-
-                    Console.WriteLine("(Binary) {0} ^ {1} = {2}", 
-                    Convert.ToString(first_number, 2), 
-                    Convert.ToString(second_number, 2), 
-                    Convert.ToString(result, 2)); 
-
-                    Console.WriteLine("(Hexadecimal) {0} ^ {1} = {2}", 
-                    Convert.ToString(first_number, 16), 
-                    Convert.ToString(second_number, 16), 
-                    Convert.ToString(result, 16));
-
-                    break;
-
-                default:
-                    Console.WriteLine("Wrong operator");
-                    break;
-            }
-
-        Console.WriteLine("Type yes to continue");
-        check = Console.ReadLine();
-
-        if(check != "yes") return;
+        for (int i=0; i <= array.Length-1; i++)
+        {
+            Console.Write("{0} ", array[i]);
         }
     }
 }
