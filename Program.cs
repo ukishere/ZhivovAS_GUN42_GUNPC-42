dc7ece042040674866345fbdfbc485547b2d0a53
ukishere <61353722+ukishere@users.noreply.github.com>
2026-04-17 15:31:52 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,57 +2,26 @@
 {
     public class Unit
     {
-        private string _name;
-        private float _health;
-
-        public Unit (string input) { Name = input; Health = 100; }
+        public Unit (string input) { Name = input; Health = 100; Damage = 5; Armor = 0.6f; }
         public Unit () : this ("Unknown unit") {}
 
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
+        private string Name { get; set; }
+        private float Health { get; set; }
+        private int Damage { get; set; }
+        private float Armor { get; set; }
 
-        public float Health
-        {
-            get { return _health; }
-            set { _health = value; }
-        }
+        public float GetRealHealth() { return Health * (1f + Armor); }
 
-        public int Damage
-        {
-            get { return 5; }
-        }
-
-        public float Armor
-        {
-            get { return 0.6f; }
-        }
-        
-        public float GetRealHealth()
-        {
-            return Health * (1f + Armor);
-        }
-
-        public bool SetDamage(int damage)
+        public bool SetDamage(float damage)
         {
             Health = Health - damage * Armor;
-
-            if (Health <= 0)
-            {
-                Health = 0;
-                return true;
-            }     
-            else 
-                return false;
+            if (Health <= 0) { Health = 0; return true; }     
+            else return false;
         }
 
-        public void Display()
-        {
-            Console.WriteLine($"Name: {_name}; Health: {Health}; Damage: {Damage}; Armor: {Armor}.");
-        }
+        public void Display() { Console.WriteLine($"Name: {Name}; Health: {Health}; Damage: {Damage}; Armor: {Armor}."); }
     }
+
     internal class Program
     {
         private static void Main(string[] args)
@@ -65,8 +34,8 @@
             Console.WriteLine("\nCreated named unit");
             b.Display();
 
-            Console.WriteLine("\nChecking his real health");
-            Console.WriteLine($"Real health: {b.GetRealHealth()}");
+            Console.WriteLine("\nChecking his health");
+            Console.WriteLine($"Health: {b.GetRealHealth()}");
 
             Console.WriteLine("\nGiving him 10 damage");
             bool check = b.SetDamage(10);
