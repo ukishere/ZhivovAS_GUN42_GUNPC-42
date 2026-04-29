94bfbe4817914fe816b3dbee8306c01e32a175f7
ukishere <61353722+ukishere@users.noreply.github.com>
2026-04-29 13:49:43 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,141 +1,212 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.VisualBasic;
 
-namespace Classes
+namespace HomeWork
 {
-    public class Unit
-    {
-        Random random = new Random();
-        //public Unit (string input) { Name = input; Health = 100; Damage = 5; Armor = 0.6f; }
-        public Unit (string input) { Name = input; Health = 100; Damage = new Interval(random.Next(0,100),random.Next(10,100)); Armor = 0.6f; }
-        public Unit () : this ("Unknown unit") {}
-
-        public float Health { get; set; }
-        private string Name { get; }
-        //private int Damage { get; }
-        private Interval Damage { get; }
-        private float Armor { get; }
-
-        public float GetRealHealth() { return Health * (1f + Armor); }
-
-        public bool SetDamage(float damage)
-        {
-            Health = Health - damage * Armor;
-            if (Health <= 0) { Health = 0; return true; }     
-            else return false;
-        }
-
-        public void Display() { Console.WriteLine($"Name: {Name}; Health: {Health}; Damage: {Damage.Min()}-{Damage.Max()}; Armor: {Armor}."); }
-    }
-
-    public class Weapon
-    {
-        private Weapon (string name) { Name = name; Durability = 1; }
-        //public Weapon (string name, int min, int max) : this (name) { SetDamageParameters(min, max); }
-        public Weapon (string name, Interval interval) : this (name) { SetDamageParameters(interval); }
-
-        private string Name { get; set; }
-        public int MinDamage { get; private set; }
-        public int MaxDamage { get; private set; }
-        private float Durability { get; }
-
-        /*public void SetDamageParameters (int min, int max)
-        {
-            if ( min > max ) { (min, max) = (max, min); Console.WriteLine($"Damage for {Name} has been set as {min}-{max}");}
-            if ( min < 1 ) { min = 1; Console.WriteLine("Minimal damage has been set as 1"); }
-            if ( max <= 1 ) { max = 10; Console.WriteLine("Maximal damage has been set as 10"); }
-
-            MinDamage = min; 
-            MaxDamage = max; 
-        }*/
-
-        public void SetDamageParameters (Interval interval)
-        {
-            MinDamage = interval.Min(); 
-            MaxDamage = interval.Max(); 
-        }
-
-        public int GetDamage()
-        {
-            return MinDamage+MaxDamage/2;
-        }
-
-        public void Display() { Console.WriteLine($"Name: {Name}; Damage: {MinDamage}-{MaxDamage}; Durability: {Durability}.\n"); }
-
-    }
-
-    public struct Interval
-    {
-        private int minValue;
-        private int maxValue;
-
-        public Interval (int min, int max)
-        {
-            minValue = min;
-            maxValue = max;
-
-            if ( minValue > maxValue ) { (minValue, maxValue) = (maxValue, minValue); Console.WriteLine("Min <=> Max");}
-            if ( minValue < 0 ) { minValue = 0; Console.WriteLine("Min has been set as 0"); }
-            if ( maxValue < 0 ) { maxValue = 0; Console.WriteLine("Max has been set as 0"); }
-            if ( minValue == maxValue ) {maxValue += 10; Console.WriteLine("Max value increased by 10"); } 
-        }
-
-        public int Min() { return minValue; }
-        public int Max() { return maxValue; }
-        public int Get() { var rand = new Random(); return rand.Next(minValue, maxValue); }
-    }
-
-    public struct Room
-    {
-        public Unit Unit { get; set; }
-        public Weapon Weapon { get; set; }
-
-        public Room (Unit unit, Weapon weapon)
-        {
-            Unit = unit;
-            Weapon = weapon;
-        }
-    }
-
-    public class Dungeon
-    {
-        Room[] rooms;
-
-        public Dungeon()
-        {
-            Random random = new Random(); 
-            rooms = new Room[random.Next(3,5)];
-            Interval interval;
-        
-            for (int i = 0; i < rooms.Length; i++)
-            {
-                interval = new Interval(random.Next(1,10), random.Next(10,100));
-                Unit unit = new Unit($"Unit {i+1}");
-                Weapon weapon = new Weapon($"Weapon {i+1}", interval);
-                rooms[i] = new Room(unit, weapon);
-            }
-        }
-
-        public void ShowRooms() 
-        { 
-            Console.WriteLine($"\nCreated {rooms.Length} rooms\n");
-
-            for (int i = 0; i < rooms.Length; i++)
-            {
-                Console.WriteLine($"Room {i+1}");
-                Console.Write("Unit - ");
-                rooms[i].Unit.Display();
-                Console.Write("Weapon - ");
-                rooms[i].Weapon.Display(); 
-            }
-        }
-    }
-
     internal class Program
     {
+        private class ListTask
+        {
+            List<string> _list = new List<string>() {"One", "Two", "Three"};
+
+            public void TaskLoop()
+            {
+                Console.WriteLine("You are in Task 1 (List)");
+                Console.WriteLine("1: Add element to the end of the list");
+                Console.WriteLine("2: Add element to the center of the list");
+                Console.WriteLine("0: Exit");
+
+                while(true)
+                {
+                    Console.Write("Your input: ");
+                    string input = Console.ReadLine();
+
+                    if (input == "1")
+                    {
+                        Console.WriteLine("Insert element: ");
+                        string element = Console.ReadLine();
+                        _list.Add(element);
+                        Display(_list);
+                    }
+                    else if (input == "2")
+                    {
+                        Console.WriteLine("Insert element: ");
+                        string element = Console.ReadLine();
+                        _list.Insert(_list.Count/2, element);
+                        Display(_list);
+                    }
+                    else if (input == "0") 
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Incorrect input");
+                    }
+                }
+            }
+       
+            private void Display(List<string> list)
+            {
+                Console.Write("Current list: ");
+                for(int i = 0; i < list.Count; i++)
+                {
+                    Console.Write($"{list[i]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private class DictionaryTask
+        {
+            Dictionary<string, float> _dictionary = new Dictionary<string, float>() 
+            {
+                {"Ivanov", 3.3f}, {"Petrov", 4.4f}, {"Sidorov", 5.0f}
+            };
+
+            public void TaskLoop()
+            {
+                Console.WriteLine("You are in Task 2 (Dictionary)");
+                Console.WriteLine("1: Add new student");
+                Console.WriteLine("2: Check student's score");
+                Console.WriteLine("0: Exit");
+
+                while(true)
+                {
+                    Console.Write("Your input: ");
+                    string input = Console.ReadLine();
+
+                    if (input == "1")
+                    {
+                        Console.WriteLine("Insert student's name: ");
+                        string name = Console.ReadLine();
+                        while (true)
+                        {
+                            Console.WriteLine("Insert student's score (2-5): ");
+                            string score = Console.ReadLine();
+                            bool check = CheckingScore(score);
+                            if (!check) { Console.WriteLine("Incorrect input. Try again."); }
+                            else { _dictionary.Add(name, float.Parse(score)); break; }
+                        }
+
+                        Display(_dictionary);
+                    }
+                    else if (input == "2")
+                    {
+                        Console.WriteLine("Insert student's name: ");
+                        string name = Console.ReadLine();
+
+                        if (_dictionary.TryGetValue(name, out float value)) 
+                        { 
+                            Console.WriteLine($"Student's score = {value}"); 
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such student"); 
+                        }
+                    }
+                    else if (input == "0") 
+                    {
+                        break;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Incorrect input");
+                    }
+                }
+            }
+       
+            private bool CheckingScore(string input)
+            {
+                if (float.TryParse(input, out float score)) { }
+                else {return false;}
+
+                if (score < 2f || score > 5f) {return false;}
+                else return true;
+            }
+        
+            private void Display(Dictionary<string, float> dictionary)
+            {
+                Console.WriteLine("Current dicrionary:");
+                foreach (KeyValuePair<string, float> pair in dictionary)
+                {
+                    Console.WriteLine($"Student: {pair.Key}; Score: {pair.Value}");
+                }
+            }
+        }
+
+        private class LinkedListTask
+        {
+            LinkedList<string> _linkedList = new LinkedList<string>();
+
+            private bool CheckingElements(string[] elements)
+            {
+                  if (elements.Count() < 3) 
+                { 
+                    Console.WriteLine("Not enough elements"); 
+                    return false;
+                }
+                else if (elements.Count() > 6) 
+                { 
+                    Console.WriteLine("Too many elements"); 
+                    return false;
+                }
+                else 
+                    return true;
+            }
+
+            public void TaskLoop()
+            {
+                Console.WriteLine("You are in Task 3 (Linked List)");
+                Console.WriteLine("Let's create a list (3-6 elements separeted with spaces)");
+                
+                while(true)
+                {
+                    Console.WriteLine("Your input (press 0 for exit): ");
+                    string input = Console.ReadLine();
+
+                    if (input == "0") break;
+
+                    string[] elements = input.Split(' ');
+                    if (CheckingElements(elements))
+                    {
+                        foreach (string element in elements)
+                        {
+                            _linkedList.AddLast(element);
+                        }
+                        
+                        Console.WriteLine("Your List starting form the beginning:");
+                        foreach (string element in _linkedList)
+                        {
+                            Console.Write($"{element} ");
+                        }
+
+                        Console.WriteLine("\nYour List starting form the end:");
+                        LinkedListNode<string> last = _linkedList.Last;
+                        while(last != null)
+                        {
+                            Console.Write($"{last.Value} ");
+                            last = last.Previous;
+                        }
+                        
+                        break;
+                    }
+                }    
+            }
+        }
+
         private static void Main(string[] args)
         {
-            Dungeon dungeon = new Dungeon();
-            dungeon.ShowRooms();
+           //Task 1
+           ListTask list = new ListTask(); 
+           list.TaskLoop();
+
+           //Task 2
+           DictionaryTask dictionary = new DictionaryTask();
+           dictionary.TaskLoop();
+
+           //Task 3 
+           LinkedListTask linkedList = new LinkedListTask();
+           linkedList.TaskLoop();
         }
     }
 }
